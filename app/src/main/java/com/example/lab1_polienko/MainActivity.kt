68dd8f97d9fa4613c44301862594ed471a83e901
artempolcreator <artempolienko123@gmail.com>
2021-10-18 12:49:57 +0300

    3

--- app/src/main/java/com/example/lab1_polienko/MainActivity.kt
+++ app/src/main/java/com/example/lab1_polienko/MainActivity.kt
@@ -2,10 +2,45 @@ package com.example.lab1_polienko
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = "States"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_1_17)
+        Log.d(TAG, "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        Log.d(TAG, "onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d(TAG, "onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d(TAG, "onStop")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy")
+        super.onDestroy()
+    }
+
+    override fun onRestart() {
+        Log.d(TAG, "onRestart")
+        super.onRestart()
     }
 }
